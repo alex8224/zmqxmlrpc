9d2e71dd514cd509413f801dcb1c4f31c02b6921
Alex zhang <zeus_paas@163.com>
2014-04-17 17:34:08 +0800

    first commit

---
+++ zmq_rpcclient.py
@@ -0,0 +1,66 @@
+# -*-coding:utf8 -*-
+
+import xmlrpclib
+from xmlrpclib import _Method, loads
+import zmq
+
+
+class ZmqTransport(xmlrpclib.Transport):
+
+    def __init__(self, uri):
+        xmlrpclib.Transport.__init__(self)
+        self.context = zmq.Context()
+        self.connection = self.context.socket(zmq.REQ)
+        self.connection.connect(uri)
+
+
+    def request(self, requestbody):
+        self.connection.send(requestbody)
+        response = self.connection.recv()
+        returnvalue, methodname = loads(response)
+        return returnvalue
+
+    def __close(self):
+        print "close~"
+        self.connection.close()
+
+class ZmqProxy(object):
+
+    def __init__(self, uri):
+
+        self.uri = uri
+        self.__transport = ZmqTransport(self.uri)
+
+
+    def __request(self, methodname, params):
+
+        body = xmlrpclib.dumps(params, methodname)
+        response = self.__transport.request(body)
+    
+        if len(response) == 1:
+            response = response[0]
+
+        return response    
+
+
+    def __getattr__(self, name):
+        return _Method(self.__request, name)
+
+
+    def __call__(self, attr):
+        if attr == "close":
+            return self.__transport.close()
+        if attr == "transport":
+            return self.__transport
+
+        raise AttributeError("Attribute %r not found" % (attr, ))
+
+    def __repr__(self):
+        return ("<ZmqProxy for %s>" % (self.uri))
+
+
+if __name__ == '__main__':
+    proxy = ZmqProxy("tcp://127.0.0.1:5556")
+    print str(proxy)
+    print proxy.hello("alexasdfasdfasdf")
+    print proxy.add(1,1)
---
+++ zmq_rpcserver.py
@@ -0,0 +1,37 @@
+import zmq
+from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
+
+class ZmqXMLRPCServer(SimpleXMLRPCDispatcher):
+    def __init__(self):
+        SimpleXMLRPCDispatcher.__init__(self)
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://*:5556")
+
+
+    def serve_forever(self):
+        while 1:
+            try:
+                request = self.socket.recv()
+                response = self._marshaled_dispatch(request)
+                # print response
+                self.socket.send(response)
+            except:
+                break
+
+        self.socket.close()    
+
+
+def hello(name):
+    return "hello %s" % name
+
+def add(x, y):
+    return x+y
+
+
+if __name__ == '__main__':
+    server = ZmqXMLRPCServer()
+    server.register_function(hello)
+    server.register_function(add)
+    server.register_function(wget)
+    server.serve_forever()
